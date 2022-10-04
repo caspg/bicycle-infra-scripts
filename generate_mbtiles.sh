aa18c028449316ab6ec89cdc05df043c80f22ba4
Kacper Golinski <kacper.golinski@gmail.com>
2022-10-04 09:06:16 +0200

    fix filtering out mtb tracsk

--- generate_mbtiles.sh
+++ generate_mbtiles.sh
@@ -27,20 +27,15 @@ osmium tags-filter data/input.osm.pbf \
 
 osmconvert data/out.osm.pbf -o=data/out.o5m
 
+# --drop-ways drops objects regardless --keep-ways
 osmfilter data/out.o5m \
   --ignore-dependencies \
-  --keep-ways="mtb:scale!=* and
-               route!=mtb and
-               ( highway=cycleway or
-                 ( bicycle=yes and highway=footway ) or
-                 ( highway!=proposed and highway!=construction and bicycle=designated ) or
-                 ( highway!=proposed and highway!=construction and cycleway=track ) or
-                 ( highway!=proposed and highway!=construction and cycleway:*=track ) or
-                 ( highway!=proposed and highway!=construction and cycleway=lane ) or
-                 ( highway!=proposed and highway!=construction and cycleway:*=lane ) )" \
+  --drop-ways="mtb:scale= route=mtb" \
+  --keep-ways="highway=cycleway or ( bicycle=yes and highway=footway ) or ( highway!=proposed and highway!=construction and bicycle=designated ) or ( highway!=proposed and highway!=construction and cycleway=track ) or ( highway!=proposed and highway!=construction and cycleway:*=track ) or ( highway!=proposed and highway!=construction and cycleway=lane ) or ( highway!=proposed and highway!=construction and cycleway:*=lane )" \
   >data/out.osm
 
 OSM_CONFIG_FILE=./osmconf.ini ogr2ogr -f GeoJSON data/out.geojson data/out.osm lines
 
 # `-z14`: Only generate zoom levels 0 through 14
+# `-l`: layer name
 tippecanoe -z14 -o data/out.mbtiles -l default --drop-fraction-as-needed data/out.geojson
